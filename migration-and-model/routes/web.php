39b3039b7e2bdc852582f912a770a3501a175581
hezrn <hezreenjayabellera@gmail.com>
2026-03-22 05:57:04 +0000

    updated the greeting page

--- migration-and-model/routes/web.php
+++ migration-and-model/routes/web.php
@@ -3,8 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
+// 1. Greet page for base URL '/'
+Route::get('/', function () {
+    // You can set a default name like 'Guest' or 'Hezreen'
+    return view('greet', ['name' => 'Hezreen']);
+});
+
+// 2. Optional greet page with a name parameter
 Route::get('/greet/{name}', function ($name) {
     return view('greet', ['name' => $name]);
 });
 
+// 3. Tasks CRUD routes
 Route::resource('tasks', TaskController::class);
